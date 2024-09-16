f802339ee6bdfe7adedc102250725901d0055697
feifly99 <148175317+feifly99@users.noreply.github.com>
2024-09-16 18:35:21 +0800

    Update Entry.c

--- Entry.c
+++ Entry.c
@@ -9,29 +9,20 @@ VOID driverUnload(PDRIVER_OBJECT DriverObject)
     IoDeleteSymbolicLink(&sybName);
     UNREFERENCED_PARAMETER(DriverObject);
 }
-
 NTSTATUS DriverEntry(PDRIVER_OBJECT driverObject, PUNICODE_STRING RegistryPath)
 {
     UNREFERENCED_PARAMETER(RegistryPath);
-    driverObject->Flags |= DO_BUFFERED_IO;
+    UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\ANYIFEI_DEVICE_NAME");
     PDEVICE_OBJECT devObj = NULL;
-    UNICODE_STRING devName = { 0 };
-    RtlInitUnicodeString(&devName, L"\\Device\\ANYIFEI_device_NAME");
-    if (NT_SUCCESS(IoCreateDevice(driverObject, 0, &devName, FILE_DEVICE_UNKNOWN, 0, TRUE, &devObj)))
-    {
-        DbgPrint("创建设备成功");
-    }
-    UNICODE_STRING sybName = { 0 };
-    RtlInitUnicodeString(&sybName, L"\\??\\ANYIFEI_SYMBOLICLINK_NAME");
-    if (NT_SUCCESS(IoCreateSymbolicLink(&sybName, &devName)))
-    {
-        DbgPrint("创建符号链接成功");
-    }
-    driverObject->DriverUnload = driverUnload;
+    IoCreateDevice(driverObject, 0, &devName, FILE_DEVICE_UNKNOWN, 0, TRUE, &devObj);
+    UNICODE_STRING symName = RTL_CONSTANT_STRING(L"\\??\\ANYIFEI_SYMBOLINK_NAME");
+    IoCreateSymbolicLink(&symName, &devName);
+    driverObject->Flags |= DO_BUFFERED_IO;
     driverObject->MajorFunction[IRP_MJ_CREATE] = myCreate;
     driverObject->MajorFunction[IRP_MJ_CLOSE] = myClose;
     driverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = Driver_User_IO_Interaction_Entry;
-    DbgPrint("go");
+    driverObject->DriverUnload = driverUnload;
+    __PLACE_HOLDER__
     return STATUS_SUCCESS;
 }
 //第一次走用户联立驱动发现停止驱动蓝屏报错SYSTEM_THREAD_XXX_NOT_HANDLED原因：设备和符号链接创建失败，导致删除了不存在的对象指针；
@@ -46,3 +37,6 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT driverObject, PUNICODE_STRING RegistryPath)
 //这里长记性：要记得加错误处理！
 
 //OPENPROCESS之后build还是不显示，原因：第一个参数传错了！
+
+//驱动停止走的是DRIVER_UNLOAD，在不交互用户的情况下记得清理DRIVER_UNLOAD的不必要的东西，比如根本就
+//不存在的符号链接！！！
