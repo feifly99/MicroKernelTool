0b891b357035f0f9b2b0053b6235b535525f48ee
feifly99 <148175317+feifly99@users.noreply.github.com>
2024-09-20 20:20:29 +0800

    Update Entry.c

--- Entry.c
+++ Entry.c
@@ -25,7 +25,6 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT driverObject, PUNICODE_STRING RegistryPath)
     __PLACE_HOLDER__
     return STATUS_SUCCESS;
 }
-
 //第一次走用户联立驱动发现停止驱动蓝屏报错SYSTEM_THREAD_XXX_NOT_HANDLED原因：设备和符号链接创建失败，导致删除了不存在的对象指针；
 //创建失败原因：没加\\Device\\前缀
 
