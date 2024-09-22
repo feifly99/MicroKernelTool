7b99a8c393e0d73daa333857e9f76aa06031731d
feifly99 <148175317+feifly99@users.noreply.github.com>
2024-09-22 16:53:18 +0800

    Update asmEntry.asm

--- asmEntry.asm
+++ asmEntry.asm
@@ -60,4 +60,75 @@
 		ret
 	__asm__getImagePathNameAddress ENDP
 
+	__asm__getExportFuncsNameByTargetIndex PROC ;_asm_getExportFuncNameByTargetIndex(ULONG64 DllBase, ULONG64 index)
+		push r11
+		push r10
+		push rbx
+		mov r11, rdx
+		mov r10, rcx
+		add rcx, 3Ch
+		mov ecx, dword ptr [rcx]
+		mov rax, r10
+		add rax, rcx 
+		add rax, 18h 
+		mov eax, dword ptr [rax + 70h]
+		mov rbx, r10
+		add rbx, rax 
+		mov ebx, dword ptr [rbx + 20h]
+		mov rcx, r10
+		add rcx, rbx
+		mov ecx, dword ptr [rcx + r11 * 4]
+		add r10, rcx
+		mov rax, r10
+		pop rbx
+		pop r10
+		pop r11
+		ret
+	__asm__getExportFuncsNameByTargetIndex ENDP
+
+	__asm__getExportFuncsAddressByTargetIndex PROC ;_asm_getExportFuncsAddressByTargetIndex(ULONG64 DllBase, ULONG64 index)
+		push r11
+		push r10
+		push rbx
+		mov r11, rdx
+		mov r10, rcx
+		add rcx, 3Ch
+		mov ecx, dword ptr [rcx]
+		mov rax, r10
+		add rax, rcx 
+		add rax, 18h 
+		mov eax, dword ptr [rax + 70h]
+		mov rbx, r10
+		add rbx, rax 
+		mov ebx, dword ptr [rbx + 1Ch]
+		mov rcx, r10
+		add rcx, rbx
+		mov ecx, dword ptr [rcx + r11 * 4]
+		add r10, rcx
+		mov rax, r10
+		pop rbx
+		pop r10
+		pop r11
+		ret
+	__asm__getExportFuncsAddressByTargetIndex ENDP
+
+	__asm__getNumberOfFunctionsExportedByName PROC ;_asm_getExportFuncsNumber(ULONG64 DllBase)
+		push r10
+		push rbx
+		mov r10, rcx
+		add rcx, 3Ch
+		mov ecx, dword ptr [rcx]
+		mov rax, r10
+		add rax, rcx 
+		add rax, 18h 
+		mov eax, dword ptr [rax + 70h]
+		mov rbx, r10
+		add rbx, rax 
+		mov ebx, dword ptr [rbx + 18h]
+		mov rax, rbx
+		pop rbx
+		pop r10
+		ret
+	__asm__getNumberOfFunctionsExportedByName ENDP
+
 END
