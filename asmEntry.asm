531149c19080de5df5009776a9c59e4b93aab460
feifly99 <148175317+feifly99@users.noreply.github.com>
2024-09-28 22:28:35 +0800

    Update asmEntry.asm

--- asmEntry.asm
+++ asmEntry.asm
@@ -59,76 +59,87 @@
 		add rax, 60h
 		ret
 	__asm__getImagePathNameAddress ENDP
+	
+	__asm__readMSR PROC ;ULONG64 __asm__readMSR(ULONG64* regArrayAddress, ULONG64 msrAddress)
+		rdmsr
+		shl rdx, 20h
+		add rax, rdx
+		ret
+	__asm__readMSR ENDP
 
-	__asm__getExportFuncsNameByTargetIndex PROC ;_asm_getExportFuncNameByTargetIndex(ULONG64 DllBase, ULONG64 index)
-		push r11
-		push r10
-		push rbx
-		mov r11, rdx
-		mov r10, rcx
+	__asm__getFuncNumsExportedTotal_Via_DllBase PROC ;__asm__getFuncNumsExportedTotal_Via_DllBase(IN PVOID dllBase, OUT SIZE_T* count);
+		push r15
+		mov r15, rcx
 		add rcx, 3Ch
 		mov ecx, dword ptr [rcx]
-		mov rax, r10
-		add rax, rcx 
-		add rax, 18h 
-		mov eax, dword ptr [rax + 70h]
-		mov rbx, r10
-		add rbx, rax 
-		mov ebx, dword ptr [rbx + 20h]
-		mov rcx, r10
-		add rcx, rbx
-		mov ecx, dword ptr [rcx + r11 * 4]
-		add r10, rcx
-		mov rax, r10
-		pop rbx
-		pop r10
-		pop r11
+		add rcx, r15 
+		add rcx, 18h
+		add rcx, 70h
+		mov ecx, dword ptr [rcx]
+		add rcx, r15
+		add rcx, 14h
+		mov ecx, dword ptr [rcx]
+		mov qword ptr [rdx], rcx
+		pop r15
 		ret
-	__asm__getExportFuncsNameByTargetIndex ENDP
+	__asm__getFuncNumsExportedTotal_Via_DllBase ENDP
 
-	__asm__getExportFuncsAddressByTargetIndex PROC ;_asm_getExportFuncsAddressByTargetIndex(ULONG64 DllBase, ULONG64 index)
-		push r11
-		push r10
-		push rbx
-		mov r11, rdx
-		mov r10, rcx
+	__asm__getFuncNumsExportedByName_Via_DllBase PROC ;__asm__getFuncNumsExportedByName_Via_DllBase(IN PVOID dllBase, OUT SIZE_T* count);
+		push r15
+		mov r15, rcx
 		add rcx, 3Ch
 		mov ecx, dword ptr [rcx]
-		mov rax, r10
-		add rax, rcx 
-		add rax, 18h 
-		mov eax, dword ptr [rax + 70h]
-		mov rbx, r10
-		add rbx, rax 
-		mov ebx, dword ptr [rbx + 1Ch]
-		mov rcx, r10
-		add rcx, rbx
-		mov ecx, dword ptr [rcx + r11 * 4]
-		add r10, rcx
-		mov rax, r10
-		pop rbx
-		pop r10
-		pop r11
+		add rcx, r15 
+		add rcx, 18h
+		add rcx, 70h
+		mov ecx, dword ptr [rcx]
+		add rcx, r15
+		add rcx, 18h
+		mov ecx, dword ptr [rcx]
+		mov qword ptr [rdx], rcx
+		pop r15
 		ret
-	__asm__getExportFuncsAddressByTargetIndex ENDP
+	__asm__getFuncNumsExportedByName_Via_DllBase ENDP
 
-	__asm__getNumberOfFunctionsExportedByName PROC ;_asm_getExportFuncsNumber(ULONG64 DllBase)
-		push r10
-		push rbx
-		mov r10, rcx
+	__asm__getFuncNameByIndex_Via_DllBase PROC ;__asm__getFuncNameByIndex_Via_DllBase(IN PVOID dllBase, IN SIZE_T index, OUT ULONG64* nameAddress);
+		push r15
+		mov r15, rcx
 		add rcx, 3Ch
 		mov ecx, dword ptr [rcx]
-		mov rax, r10
-		add rax, rcx 
-		add rax, 18h 
-		mov eax, dword ptr [rax + 70h]
-		mov rbx, r10
-		add rbx, rax 
-		mov ebx, dword ptr [rbx + 18h]
-		mov rax, rbx
-		pop rbx
-		pop r10
+		add rcx, r15 
+		add rcx, 18h
+		add rcx, 70h
+		mov ecx, dword ptr [rcx]
+		add rcx, r15
+		add rcx, 20h
+		mov ecx, dword ptr [rcx]
+		add rcx, r15
+		mov ecx, dword ptr [rcx + rdx * 4]
+		add rcx, r15
+		mov qword ptr [r8], rcx
+		pop r15
 		ret
-	__asm__getNumberOfFunctionsExportedByName ENDP
+	__asm__getFuncNameByIndex_Via_DllBase ENDP
+
+	__asm__getFuncAddressByIndex_Via_DllBase PROC ;__asm__getFuncAddressByIndex_Via_DllBase(IN PVOID dllBase, IN SIZE_T differWhetherNameExported, IN SIZE_T index, OUT ULONG64* funcAddress);
+		push r15
+		add rdx, r8
+		mov r15, rcx
+		add rcx, 3Ch
+		mov ecx, dword ptr [rcx]
+		add rcx, r15 
+		add rcx, 18h
+		add rcx, 70h
+		mov ecx, dword ptr [rcx]
+		add rcx, r15
+		add rcx, 1Ch
+		mov ecx, dword ptr [rcx]
+		add rcx, r15
+		mov ecx, dword ptr [rcx + rdx * 4]
+		add rcx, r15
+		mov qword ptr [r9], rcx
+		pop r15
+		ret
+	__asm__getFuncAddressByIndex_Via_DllBase ENDP
 
 END
