cdc5508f66f4eeb7cc13a65c0e0664405ba60587
feifly99 <148175317+feifly99@users.noreply.github.com>
2024-10-13 16:26:25 +0800

    Update asmEntry.asm

--- asmEntry.asm
+++ asmEntry.asm
@@ -1,4 +1,39 @@
+.data
+
+	t1 QWORD 7FFD513C0542h
+	t2 QWORD 7FFD51531530h
+	t3 QWORD 7FFD5152A728h
+	t4 QWORD 7FFD5141E4D0h
+	t5 QWORD 7FFD51379EB0h
+
 .code
+	
+	__asm__testProc PROC
+		mov qword ptr [rsp + 8], rbx
+		mov qword ptr [rsp + 10h], rsi
+		push rdi
+		sub rsp, 20h
+		mov rdi, rcx
+		test rcx, rcx
+		je labelss
+		lea rdx, qword ptr [t2]
+		call qword ptr [t3]
+		nop dword ptr [rax + rax]
+	labelss:
+		xor r8d, r8d
+		xor rcx, rdi
+		call t5
+		mov rbx, qword ptr [rsp + 30h]
+		mov rsi, qword ptr [rsp + 38h]
+		add rsp, 20h
+		pop rdi
+		ret
+	__asm__testProc ENDP
+
+	__asm__readRCX PROC
+		mov rax, rcx
+		ret
+	__asm__readRCX ENDP
 
 	__asm__readDR0 PROC
 		mov rax, dr0
@@ -10,7 +45,6 @@
 		ret
 	__asm__readCR0 ENDP
 
-	;___X64___Only___↓___
 	__asm__WRbreak PROC ; ULONG64 __asm__WRbreak(IN ULONG64* oldCR0Address)
 		mov rax, cr0
 		mov qword ptr [rcx], rax
@@ -35,8 +69,6 @@
 		mov cr3, rcx
 		ret
 	__asm__PDTrestore ENDP
-	
-	;___X64___Only___↑___
 
 	__asm__getEFLregistor PROC
 		pushfq
@@ -60,14 +92,14 @@
 		ret
 	__asm__getImagePathNameAddress ENDP
 	
-	__asm__readMSR PROC ;ULONG64 __asm__readMSR(ULONG64* regArrayAddress, ULONG64 msrAddress)
+	__asm__readMSR PROC ;ULONG64 __asm__readMSR(IN ULONG64* regArrayAddress, OUT ULONG64 msrAddress)
 		rdmsr
 		shl rdx, 20h
 		add rax, rdx
 		ret
 	__asm__readMSR ENDP
 
-	__asm__getFuncNumsExportedTotal_Via_DllBase PROC ;__asm__getFuncNumsExportedTotal_Via_DllBase(IN PVOID dllBase, OUT SIZE_T* count);
+	__asm__getFuncNumsExportedTotal_Via_DllBase PROC ;__asm__getFuncNumsExportedTotal_Via_DllBase(IN PVOID dllBase);
 		push r15
 		mov r15, rcx
 		add rcx, 3Ch
@@ -79,12 +111,12 @@
 		add rcx, r15
 		add rcx, 14h
 		mov ecx, dword ptr [rcx]
-		mov qword ptr [rdx], rcx
+		mov rax, rcx
 		pop r15
 		ret
 	__asm__getFuncNumsExportedTotal_Via_DllBase ENDP
 
-	__asm__getFuncNumsExportedByName_Via_DllBase PROC ;__asm__getFuncNumsExportedByName_Via_DllBase(IN PVOID dllBase, OUT SIZE_T* count);
+	__asm__getFuncNumsExportedByName_Via_DllBase PROC ;__asm__getFuncNumsExportedByName_Via_DllBase(IN PVOID dllBase);
 		push r15
 		mov r15, rcx
 		add rcx, 3Ch
@@ -96,12 +128,12 @@
 		add rcx, r15
 		add rcx, 18h
 		mov ecx, dword ptr [rcx]
-		mov qword ptr [rdx], rcx
+		mov rax, rcx
 		pop r15
 		ret
 	__asm__getFuncNumsExportedByName_Via_DllBase ENDP
 
-	__asm__getFuncNameByIndex_Via_DllBase PROC ;__asm__getFuncNameByIndex_Via_DllBase(IN PVOID dllBase, IN SIZE_T index, OUT ULONG64* nameAddress);
+	__asm__getFuncNameByIndex_Via_DllBase PROC ;__asm__getFuncNameByIndex_Via_DllBase(IN PVOID dllBase, IN SIZE_T index);
 		push r15
 		mov r15, rcx
 		add rcx, 3Ch
@@ -116,12 +148,12 @@
 		add rcx, r15
 		mov ecx, dword ptr [rcx + rdx * 4]
 		add rcx, r15
-		mov qword ptr [r8], rcx
+		mov rax, rcx
 		pop r15
 		ret
 	__asm__getFuncNameByIndex_Via_DllBase ENDP
 
-	__asm__getFuncAddressByIndex_Via_DllBase PROC ;__asm__getFuncAddressByIndex_Via_DllBase(IN PVOID dllBase, IN SIZE_T differWhetherNameExported, IN SIZE_T index, OUT ULONG64* funcAddress);
+	__asm__getFuncAddressByIndex_Via_DllBase PROC ;__asm__getFuncAddressByIndex_Via_DllBase(IN PVOID dllBase, IN SIZE_T differWhetherNameExported, IN SIZE_T index);
 		push r15
 		add rdx, r8
 		mov r15, rcx
@@ -137,7 +169,7 @@
 		add rcx, r15
 		mov ecx, dword ptr [rcx + rdx * 4]
 		add rcx, r15
-		mov qword ptr [r9], rcx
+		mov rax, rcx
 		pop r15
 		ret
 	__asm__getFuncAddressByIndex_Via_DllBase ENDP
