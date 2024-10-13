83492bdd48402cf1edbdb2b557eaf5e989b2e7f4
feifly99 <148175317+feifly99@users.noreply.github.com>
2024-10-13 16:26:43 +0800

    Update DebugeeSource.c

--- DebugeeSource.c
+++ DebugeeSource.c
@@ -474,6 +474,40 @@ VOID buildDoubleLinkedAddressListForPatternStringByKMPAlgorithm(
     KeUnstackDetachProcess(&apc);
     ObDereferenceObject(pe);
 }
+VOID DbgPrintF(float* floatNumPointer)
+{
+    INT integerPart = 0;
+    ULONG64 fractionalPart = 0;
+    UINT f = *(UINT*)floatNumPointer;
+    UINT sign = (f >> 31) & 1;
+    UINT exponent = (f >> 23) & 0xFF;
+    UINT mantissa = f & 0x7FFFFF;
+    INT exp = exponent - 127;
+    float value = (float)0.0;
+    float fractionalValue = (float)0.0;
+    if (exponent == 0)
+    {
+        value = (float)(mantissa) / (1 << 23);
+    }
+    else
+    {
+        value = 1.0f + (float)(mantissa) / (1 << 23);
+    }
+    value = value * (float)(1 << exp);
+    if (sign)
+    {
+        value = -value;
+    }
+    integerPart = (INT)value;
+    if (value < 0)
+    {
+        value = -value;
+    }
+    fractionalValue = value - (float)integerPart;
+    fractionalPart = (ULONG64)(fractionalValue * 100000000);
+    DbgPrint("%d.%llu\n", integerPart, fractionalPart);
+    return;
+}
 VOID displayAllModuleInfomationByProcessId(
     IN ULONG64 pid
 )
@@ -563,7 +597,6 @@ ULONG64 getPointerToSSDT(
     ULONG64 pointerToSSDT = currentRIPAddress + keyOffset;
     return pointerToSSDT;
 }
-
 ULONG64 getAvaliableExecuteMemoryInSSDT(
 
 )
@@ -575,7 +608,6 @@ ULONG64 getAvaliableExecuteMemoryInSSDT(
     ULONG64 upper13BytesAddress = NtOpenProcessAddress - 13;
     return upper13BytesAddress;
 }
-
 ULONG64 getSSDTFunctionAddressByIndex(
     IN ULONG64 index
 )
@@ -583,6 +615,132 @@ ULONG64 getSSDTFunctionAddressByIndex(
     ULONG64 pointerToSSDT = getPointerToSSDT();
     ULONG64 SSDT_BASE = *(ULONG64*)pointerToSSDT;
     return SSDT_BASE + (ULONG64)((*(ULONG*)(SSDT_BASE + index * 4)) >> 4);
+}
+ULONG64 getPIDByProcessName(
+    IN PUCHAR name
+)
+{
+    ULONG64 pe = (ULONG64)IoGetCurrentProcess();
+    ULONG64 head = pe;
+    ULONG64 listEntryOffset = 0x448;
+    ULONG64 uniqueProcessIdOffset = 0x440;
+    ULONG64 imageFileNameOffset = 0x5A8;
+    SIZE_T maxHoldenLength = 15;
+    pe = (ULONG64)(((PLIST_ENTRY)(pe + listEntryOffset))->Flink) - listEntryOffset;
+    while (pe != head)
+    {
+        if (strncmp((PVOID)(pe + imageFileNameOffset), (PVOID)name, min(strlen((CONST CHAR*)name), maxHoldenLength) - 1) != 0)
+        {
+            //imageFileName如果字符个数大于等于15，那么最后一位一定是\0.
+            //所以比较时，如果输入字符串长度大于等于15，那么要比较前14个字节就行.
+            pe = (ULONG64)(((PLIST_ENTRY)(pe + listEntryOffset))->Flink) - listEntryOffset;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return *(ULONG64*)(pe + uniqueProcessIdOffset);
+}
+ULONG64 getDllInLoadAddress(
+    IN HANDLE pid,
+    IN PUNICODE_STRING dllName
+)
+{
+    PEPROCESS pe = NULL;
+    PsLookupProcessByProcessId((HANDLE)pid, &pe);
+    KAPC_STATE apc = { 0 };
+    KeStackAttachProcess(pe, &apc);
+    ULONG64 pebAddress = (ULONG64)pe + 0x550;
+    ULONG64 peb = *(ULONG64*)pebAddress;
+    ULONG64 pldAddress = peb + 0x18;
+    ULONG64 pld = *(ULONG64*)pldAddress;
+    ULONG64 InLoadOrderModuleListAddress = (ULONG64)pld + 0x10;
+    PLIST_ENTRY initialEntryAddress = (PLIST_ENTRY)InLoadOrderModuleListAddress;
+    PLIST_ENTRY temp = initialEntryAddress;
+    ULONG64 ret = 0x0;
+    while ((UL64)temp != (ULONG64)initialEntryAddress->Blink)
+    {
+        if(RtlCompareUnicodeString(dllName, (PUNICODE_STRING)((ULONG64)temp->Flink + 0x58), TRUE) == 0)
+        {
+            ret =  *(ULONG64*)((ULONG64)temp->Flink + 0x30);
+            break;
+        }
+        temp = temp->Flink;
+    }
+    KeUnstackDetachProcess(&apc);
+    ObDereferenceObject(pe);
+    return ret;
+}
+VOID displayDllExportFunctionTable(
+    IN HANDLE pid,
+    IN PVOID dllBaseInLoad
+)
+{
+    PEPROCESS pe = NULL;
+    PsLookupProcessByProcessId((HANDLE)pid, &pe);
+    KAPC_STATE apc = { 0 };
+    KeStackAttachProcess(pe, &apc);
+    SIZE_T sizeTOTAL = __asm__getFuncNumsExportedTotal_Via_DllBase(dllBaseInLoad);
+    SIZE_T sizeNAME = __asm__getFuncNumsExportedByName_Via_DllBase(dllBaseInLoad);
+    SIZE_T diff = 0;
+    if (sizeTOTAL >= sizeNAME)
+    {
+        diff = sizeTOTAL - sizeNAME;
+    }
+    for (SIZE_T j = 0; j < sizeNAME; j++)
+    {
+        DbgPrint("Func Name: %s, Func Address: 0x%p", __asm__getFuncNameByIndex_Via_DllBase(dllBaseInLoad, j), __asm__getFuncAddressByIndex_Via_DllBase(dllBaseInLoad, diff, j));
+    }
+    KeUnstackDetachProcess(&apc);
+    ObDereferenceObject(pe);
+    return;
+}
+ULONG64 getDllExportFunctionAddressByName(
+    IN HANDLE pid,
+    IN PVOID dllBaseInLoad,
+    IN PUCHAR funcName
+)
+{
+    PEPROCESS pe = NULL;
+    PsLookupProcessByProcessId((HANDLE)pid, &pe);
+    KAPC_STATE apc = { 0 };
+    KeStackAttachProcess(pe, &apc);
+    SIZE_T sizeTOTAL = __asm__getFuncNumsExportedTotal_Via_DllBase(dllBaseInLoad);
+    SIZE_T sizeNAME = __asm__getFuncNumsExportedByName_Via_DllBase(dllBaseInLoad);
+    SIZE_T diff = 0;
+    ULONG64 ret = 0x0;
+    if (sizeTOTAL >= sizeNAME)
+    {
+        diff = sizeTOTAL - sizeNAME;
+    }
+    for (SIZE_T j = 0; j < sizeNAME; j++)
+    {
+        if (strncmp((CONST CHAR*)funcName,(CONST CHAR*)__asm__getFuncNameByIndex_Via_DllBase(dllBaseInLoad, j), min(strlen((CONST CHAR*)funcName),strlen((CONST CHAR*)__asm__getFuncNameByIndex_Via_DllBase(dllBaseInLoad, j))) - 1) == 0)
+        {
+            ret = (ULONG64)__asm__getFuncAddressByIndex_Via_DllBase(dllBaseInLoad, diff, j);
+            break;
+        }
+    }
+    KeUnstackDetachProcess(&apc);
+    ObDereferenceObject(pe);
+    return ret;
+}
+UCHAR readByte(
+    IN HANDLE pid, 
+    IN PVOID address
+)
+{
+    UCHAR targetByte = 0x00;
+    PEPROCESS pe = NULL;
+    PsLookupProcessByProcessId(pid, &pe);
+    KAPC_STATE apc = { 0 };
+    KeStackAttachProcess(pe, &apc);
+    memcpy(&targetByte, address, 1);
+    DbgPrint("%hhx", targetByte);
+    KeUnstackDetachProcess(&apc);
+    ObDereferenceObject(pe);
+    return targetByte;
 }
 VOID writeProcessMemory(
     IN ULONG64 pid,
@@ -671,6 +829,7 @@ VOID restoreHiddenProcess(
     thisNodeEntryAddress->Flink = thisNodeNextNodeEntryAddress;
     thisNodeNextNodeEntryAddress->Blink = thisNodeEntryAddress;
     thisNodeEntryAddress->Blink = thisNodePrevNodeEntryAddress;
+    return;
 }
 VOID processPretentProcedure(
     IN HANDLE dirtyPID,
@@ -746,6 +905,22 @@ VOID restorePretentProcess(
     //KeBugCheckEx(0x33333333, 0, 0, 0, 0);
     __asm__WRrestore(oldCR0);
     ObDereferenceObject(dirtyPE);
+}
+VOID hideThisDriver(
+    IN PDRIVER_OBJECT driverObject
+)
+{
+    ((PLIST_ENTRY)(driverObject->DriverSection))->Blink->Flink = ((PLIST_ENTRY)(driverObject->DriverSection))->Flink;
+    ((PLIST_ENTRY)(driverObject->DriverSection))->Flink->Blink = ((PLIST_ENTRY)(driverObject->DriverSection))->Blink;
+    return;
+}
+VOID restoreThisDriver(
+    IN PDRIVER_OBJECT driverObject
+)
+{
+    ((PLIST_ENTRY)(driverObject->DriverSection))->Flink->Blink = (PLIST_ENTRY)(driverObject->DriverSection);
+    ((PLIST_ENTRY)(driverObject->DriverSection))->Blink->Flink = (PLIST_ENTRY)(driverObject->DriverSection);
+    return;
 }
 ULONG hookSSDTProcedure(
     IN ULONG64 functionIndexInSSDT,
