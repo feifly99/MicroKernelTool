6b06ee5110c86f18120d464b28d73b1fd25c9998
feifly99 <148175317+feifly99@users.noreply.github.com>
2025-02-10 20:44:52 +0800

    Update main.c

--- main.c
+++ main.c
@@ -7,16 +7,28 @@ CONST INT _fltused = 0;
 
 VOID driverUnload(PDRIVER_OBJECT driverObject)
 {
-    UNREFERENCED_PARAMETER(driverObject);
+    UNICODE_STRING deviceSymbolicName = { 0 };
+    RtlInitUnicodeString(&deviceSymbolicName, L"\\??\\ANYIFEI_SYMBOLINK_NAME");
+    IoDeleteSymbolicLink(&deviceSymbolicName);
+    IoDeleteDevice(driverObject->DeviceObject);
     DbgPrint("Driver Unload");
+    return;
 }
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT driverObject, PUNICODE_STRING reg_path)
 {
     UNREFERENCED_PARAMETER(reg_path);
     driverObject->DriverUnload = driverUnload;
-
-    UNICODE_STRING dllFullPath = RTL_CONSTANT_STRING(L"D:\\testDLL.dll");
-    dllInjectionByRemoteThread((PUCHAR)"League of Legends.exe", &dllFullPath);
+    driverObject->Flags |= DO_BUFFERED_IO;
+    PDEVICE_OBJECT devObj = NULL;
+    UNICODE_STRING deviceName = { 0 };
+    RtlInitUnicodeString(&deviceName, L"\\Device\\ANYIFEI_device_NAME");
+    UNICODE_STRING deviceSymbolicName = { 0 };
+    RtlInitUnicodeString(&deviceSymbolicName, L"\\??\\ANYIFEI_SYMBOLINK_NAME");
+    IoCreateDevice(driverObject, 0, &deviceName, FILE_DEVICE_UNKNOWN, 0, TRUE, &devObj);
+    IoCreateSymbolicLink(&deviceSymbolicName, &deviceName);
+    driverObject->MajorFunction[IRP_MJ_CREATE] = myCreate;
+    driverObject->MajorFunction[IRP_MJ_CLOSE] = myClose;
+    driverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = Driver_User_IO_Interaction_Entry;
     return STATUS_SUCCESS;
 }
