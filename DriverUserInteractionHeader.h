089407512c4eb3a4bbab53266c5473c21bcf8bc0
feifly99 <148175317+feifly99@users.noreply.github.com>
2025-02-10 20:40:14 +0800

    Update DriverUserInteractionHeader.h

--- DriverUserInteractionHeader.h
+++ DriverUserInteractionHeader.h
@@ -8,19 +8,16 @@
 
 //Driver_User shared macro/struct and pointer 
 
-#define ____$_INITIZE_PROCESS_HANDLE_$____					CTL_CODE(FILE_DEVICE_UNKNOWN, 0x8001, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define ____$_INITIZE_PROCESS_ID_$____						CTL_CODE(FILE_DEVICE_UNKNOWN, 0x8001, METHOD_BUFFERED, FILE_ANY_ACCESS)
 #define ____$_INITIALIZE_PROCESS_MEMORY_SPACE_$____         CTL_CODE(FILE_DEVICE_UNKNOWN, 0x8002, METHOD_BUFFERED, FILE_ANY_ACCESS)
 #define ____$_SEARCH_PROCEDURE_$____						CTL_CODE(FILE_DEVICE_UNKNOWN, 0x8003, METHOD_BUFFERED, FILE_ANY_ACCESS)
-#define ____$_STOP_SEARCH_PATTERN_$____						CTL_CODE(FILE_DEVICE_UNKNOWN, 0x8004, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define ____$_STOP_SEARCH_PROCEDURE_$____					CTL_CODE(FILE_DEVICE_UNKNOWN, 0x8004, METHOD_BUFFERED, FILE_ANY_ACCESS)
 #define ____$_LIST_PROCESS_MODULE_$____						CTL_CODE(FILE_DEVICE_UNKNOWN, 0x8005, METHOD_BUFFERED, FILE_ANY_ACCESS)
 #define ____$_LIST_PROCESS_THREAD_$____						CTL_CODE(FILE_DEVICE_UNKNOWN, 0x8006, METHOD_BUFFERED, FILE_ANY_ACCESS)
 #define ____$_WRITE_PROCESS_MEMORY_$____					CTL_CODE(FILE_DEVICE_UNKNOWN, 0x8007, METHOD_BUFFERED, FILE_ANY_ACCESS)
 #define ____$_PROCESS_HIDEN_PROCEDURE_$____					CTL_CODE(FILE_DEVICE_UNKNOWN, 0x8008, METHOD_BUFFERED, FILE_ANY_ACCESS)
 #define ____$_PROCESS_PRETENT_PROCEDURE_$____				CTL_CODE(FILE_DEVICE_UNKNOWN, 0x8009, METHOD_BUFFERED, FILE_ANY_ACCESS)
-#define ____$_UNLOAD_DRIVER_PREPARE_$____					CTL_CODE(FILE_DEVICE_UNKNOWN, 0x8010, METHOD_BUFFERED, FILE_ANY_ACCESS)
-
-#define USER_IN		
-#define DRIVER_OUT	
+#define ____$_UNLOAD_DRIVER_PREPARE_$____					CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800A, METHOD_BUFFERED, FILE_ANY_ACCESS)
 
 #define __INIT_GLOBAL__DEFINES__
 #define __PROCESS_MEMORY_SPACE_DEFINES__
@@ -33,51 +30,14 @@
 #define ______BASIC_MAJOR_FUNCTION______
 #define ______LONG_FUNCTION_EXTRACT______
 
-#define __SEARCH_BYTE__ 0
-#define __SEARCH_WORD__ 1
-#define __SEARCH_DWORD__ 2
-#define __SEARCH_QWORD__ 3
-#define __SEARCH_FLOAT__ 4
-#define __SEARCH_DOUBLE__ 5
-#define __SEARCH_PATTERN__ 6
-
-#define IOCTL_COMPLETE_MARK(status, len) \
+#define IOCTL_COMPLETE_MARK(retSt, status, len) \
 do\
 {\
 	pIrp->IoStatus.Status = status;\
 	pIrp->IoStatus.Information = len;\
 	IoCompleteRequest(pIrp, IO_NO_INCREMENT);\
-}while (0); \
-
-/*
-
-	#define __FIRST_PRECISE_SCAN__ 0x80
-	#define __FIRST_FUZZY_SCAN__ 0x90
-
-	#define __TYPE_BYTE__ 0xA1
-	#define __TYPE_WORD__ 0xA2
-	#define __TYPE_DWORD__ 0xA3
-	#define __TYPE_QWORD__ 0xA4
-	#define __TYPE_PATTERN__ 0xA5
-	#define __TYPE_FLOAT__ 0xA6
-	#define __TYPE_DOUBLE__ 0xA7
-	#define __TYPE_NOT_FLOATING__ 0xA8
-
-	#define __Continue_PRECISE__ 0xC1
-	#define __Continue_LARGER__ 0xC2
-	#define __Continue_LOWER__ 0xC3
-	#define __Continue_UNCHANGED__ 0xC4
-	#define __Continue_REGION__ 0xC5
-
-*/
-
-typedef struct _Driver_User_SearchModeInput
-{
-	BOOLEAN isFirstScan;
-	UCHAR dataType; //begin with __TYPE_
-	UCHAR scanMode; //begin with __FIRST_ or __Continue_
-	PSMI smi;
-}D_U_SMI, *PD_U_SMI;
+	retSt = status;\
+}while (0) \
 
 typedef struct _WriteProcessMemoryInput
 {
@@ -112,3 +72,5 @@ ULONG checkProtectAttributesForTargetAddress(
 	PVOID targetAddress
 );
 #endif
+//使用枚举和宏表达状态参数、结构内嵌联合实现伪多态（CUDA也是）
+//熟悉宏操作（保护宏、##、#、@等）
